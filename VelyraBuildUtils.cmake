4d8722c10fd949ce68a4744ceaf65f859f45568e
artick788 <60552965+artick788@users.noreply.github.com>
2025-11-08 11:09:48 +0100

    Wrong CMake includes

--- VelyraBuildUtils.cmake
+++ VelyraBuildUtils.cmake
@@ -1,3 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/Configuration.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/PrettyColors.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/CompilerFlags.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/FetchLibs.cmake)
