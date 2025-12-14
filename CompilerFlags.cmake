b82129089ff0d10c01057967e74b21359fa0a8d7
artick788 <60552965+artick788@users.noreply.github.com>
2025-12-14 18:11:37 +0100

    Supress MSVC warning regarding DLL boundaries

--- CompilerFlags.cmake
+++ CompilerFlags.cmake
@@ -14,6 +14,7 @@ endif ()
 if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     # Flag for recursive macro expansion
     add_compile_options(/Zc:preprocessor)
+    add_compile_options(/wd4251) # Prevents warning for DLL boundaries, fix it later
 endif ()
 
 # Address Sanitizer
